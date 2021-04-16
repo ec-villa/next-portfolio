c1ac6017c542eabbe489587fa8b2f0ae56f608bb
Henry Villa <h.villa@signum-consult.com>
2021-04-16 12:28:44 +0200

    adjust text in home page

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Layout from '../components/Layout';
 
 const Index = () => (
     <Layout title='Home'>
-        <p>Welcome to the Home page</p>
+        <p>Welcome to the my Profile Page</p>
     </Layout>
 );
 
